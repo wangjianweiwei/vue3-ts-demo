768922b55f1e689371709326160f856ec0210b19
wangjianwei <jianwei.wang@lionmed.cn>
2023-03-23 23:42:06 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+createApp(App).use(router).use(ElementPlus).mount('#app')
---
+++ src/views/LoginView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login-box">
+
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.login-box {
+  width: 100%;
+  height: 100%;
+  background: url("@/assets/bg.png") no-repeat;
+  background-size: 100% 100%;;
+}
+</style>
